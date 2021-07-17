a0ae8b35be686d2e000fa85bffa22410a97ea4d6
hadi jutt <50537823+Hadi-jutt@users.noreply.github.com>
2021-07-17 18:02:10 +0500

    Add files via upload

---
+++ Netwroker Scanner/Netwroker Scanner/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Netwroker_Scanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Scan = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.IpAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HostName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MacAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Company = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.IPADD = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Scan
+            // 
+            this.Scan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Scan.AutoSize = true;
+            this.Scan.Location = new System.Drawing.Point(1011, 33);
+            this.Scan.Name = "Scan";
+            this.Scan.Size = new System.Drawing.Size(75, 45);
+            this.Scan.TabIndex = 0;
+            this.Scan.Text = "Scan";
+            this.Scan.UseVisualStyleBackColor = true;
+            this.Scan.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(981, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IpAddress,
+            this.HostName,
+            this.MacAddress,
+            this.Company});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 84);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1082, 365);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // IpAddress
+            // 
+            this.IpAddress.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.IpAddress.FillWeight = 3.944107F;
+            this.IpAddress.Frozen = true;
+            this.IpAddress.HeaderText = "IP Address";
+            this.IpAddress.Name = "IpAddress";
+            this.IpAddress.ReadOnly = true;
+            this.IpAddress.Width = 200;
+            // 
+            // HostName
+            // 
+            this.HostName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.HostName.FillWeight = 0.7512166F;
+            this.HostName.Frozen = true;
+            this.HostName.HeaderText = "Host Name";
+            this.HostName.Name = "HostName";
+            this.HostName.ReadOnly = true;
+            this.HostName.Width = 200;
+            // 
+            // MacAddress
+            // 
+            this.MacAddress.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.MacAddress.FillWeight = 0.7512166F;
+            this.MacAddress.Frozen = true;
+            this.MacAddress.HeaderText = "MAC Address";
+            this.MacAddress.Name = "MacAddress";
+            this.MacAddress.ReadOnly = true;
+            this.MacAddress.Width = 200;
+            // 
+            // Company
+            // 
+            this.Company.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Company.FillWeight = 2.323351F;
+            this.Company.Frozen = true;
+            this.Company.HeaderText = "Company name";
+            this.Company.Name = "Company";
+            this.Company.ReadOnly = true;
+            this.Company.Width = 200;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // IPADD
+            // 
+            this.IPADD.AutoSize = true;
+            this.IPADD.Location = new System.Drawing.Point(694, 81);
+            this.IPADD.Name = "IPADD";
+            this.IPADD.Size = new System.Drawing.Size(46, 17);
+            this.IPADD.TabIndex = 3;
+            this.IPADD.Text = "label1";
+            this.IPADD.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(567, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1098, 449);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IPADD);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Scan);
+            this.Name = "Form1";
+            this.Text = "Lan Scanner";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Scan;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label IPADD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IpAddress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HostName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MacAddress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Company;
+    }
+}
+
---
+++ Netwroker Scanner/Netwroker Scanner/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Netwroker_Scanner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Thread.Sleep(500);
+            Ping ping;
+            IPAddress addr;
+            PingReply pingReply;
+            IPHostEntry host;
+            string name;
+
+             
+            Parallel.For(0, 254, (i, loopstate) =>
+            {
+                ping = new Ping();
+                pingReply = ping.Send(textBox1.Text + i.ToString());
+                this.BeginInvoke((Action)delegate()
+                {
+                    if (pingReply.Status == IPStatus.Success)
+                    {
+                        try
+                        {
+                            addr = IPAddress.Parse(textBox1.Text + i.ToString());
+                            host = Dns.GetHostEntry(addr);
+                            name = host.HostName;
+
+                            dataGridView1.Rows.Add();
+                            int nRowIndex = dataGridView1.Rows.Count - 1;
+                            dataGridView1.Rows[nRowIndex].Cells[0].Value = textBox1.Text + i.ToString();
+                            dataGridView1.Rows[nRowIndex].Cells[1].Value = name;
+                            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+                            string macAddress;
+                            pProcess.StartInfo.FileName = "arp";
+                            pProcess.StartInfo.Arguments = "-a " + dataGridView1.Rows[nRowIndex].Cells[0].Value.ToString();
+                            pProcess.StartInfo.UseShellExecute = false;
+                            pProcess.StartInfo.RedirectStandardOutput = true;
+                            pProcess.StartInfo.CreateNoWindow = true;
+                            pProcess.Start();
+                            string strOutput = pProcess.StandardOutput.ReadToEnd();
+                            string[] substrings = strOutput.Split('-');
+                            if (substrings.Length >= 8)
+                            {
+                                macAddress = substrings[3].Substring(Math.Max(0, substrings[3].Length - 2))
+                                         + "-" + substrings[4] + "-" + substrings[5] + "-" + substrings[6]
+                                         + "-" + substrings[7] + "-"
+                                         + substrings[8].Substring(0, 2);
+                                dataGridView1.Rows[nRowIndex].Cells[2].Value = macAddress;
+                                // return macAddress;
+
+                                label1.Text = macAddress;
+                                string IPP;
+                                IPP = label1.Text;
+                                IPP = IPP.Remove(IPP.Length - 9);
+                                switch (IPP)
+                                {
+                                    case "98-E7-43":
+                                        dataGridView1.Rows[nRowIndex].Cells[3].Value = "Dell INC";
+                                        break;
+                                    case "C8-F7-50":
+                                        dataGridView1.Rows[nRowIndex].Cells[3].Value = "Dell Inc";
+                                        break;
+                                    case "6C-2B-59":
+                                        dataGridView1.Rows[nRowIndex].Cells[3].Value = "Dell Inc";
+                                        break;
+                                    case "c0-b8-83":
+                                        dataGridView1.Rows[nRowIndex].Cells[3].Value = "HP";
+                                        break;
+                                    case "88-3A-30":
+                                        dataGridView1.Rows[nRowIndex].Cells[3].Value = "Hewlett Packard Enterprise Company";
+                                        break;
+                                    case "D4-6B-A6":
+                                        dataGridView1.Rows[nRowIndex].Cells[3].Value = "HUAWEI TECHNOLOGIES CO.,LTD";
+                                        break;
+                                    case "D0-43-1E":
+                                        dataGridView1.Rows[nRowIndex].Cells[3].Value = "Dell Inc";
+                                        break;
+                                    case "E4-54-E8":
+                                        dataGridView1.Rows[nRowIndex].Cells[3].Value = "Dell INC";
+                                        break;
+                                    case "8C-04-BA":
+                                        dataGridView1.Rows[nRowIndex].Cells[3].Value = "Dell Inc";
+                                        break;
+                                    case "F0-D4-E2":
+                                        dataGridView1.Rows[nRowIndex].Cells[3].Value = "Dell Inc";
+                                        break;
+                                    case "E4-43-4B":
+                                        dataGridView1.Rows[nRowIndex].Cells[3].Value = "Dell Inc";
+                                        break;
+                                    case "D0-67-E5":
+                                        dataGridView1.Rows[nRowIndex].Cells[3].Value = "Dell Inc";
+                                        break;
+                                    case "28-F1-0E":
+                                        dataGridView1.Rows[nRowIndex].Cells[3].Value = "Dell inc";
+                                        break;
+                                    case "6c-d9-4c":
+                                        dataGridView1.Rows[nRowIndex].Cells[3].Value = "Vivo";
+                                        break;
+                                    default:
+                                        dataGridView1.Rows[nRowIndex].Cells[3].Value = "Vender not found";
+                                        break;
+                                }
+                            }
+
+                            else
+                            {
+                                //  return "not found";
+                            }
+                            // dataGridView1.Rows[nRowIndex].Cells[2].Value = "Active";
+                        }
+                        catch (SocketException ex)
+                        {
+                            name = "?";
+
+                        }
+                    }
+                });
+
+            });
+            MessageBox.Show("Scan complete");
+            var macAddr =
+        (
+            from nic in NetworkInterface.GetAllNetworkInterfaces()
+            where nic.OperationalStatus == OperationalStatus.Up
+            select nic.GetPhysicalAddress().ToString()
+        ).FirstOrDefault();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPHostEntry host;
+            string localIP = "?";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+                if (ip.AddressFamily.ToString() == "InterNetwork")
+                {
+                    string IPP;
+                    IPP = localIP = ip.ToString();
+                    string[] alloctates = IPP.Split('.');
+
+                    foreach (string str in alloctates)
+                    {
+                        IPADD.Text = IPADD.Text + str + "\n";
+                    }
+
+                    int value1 = Int32.Parse(alloctates[0]);
+                    int value2 = Int32.Parse(alloctates[1]);
+                    int value3 = Int32.Parse(alloctates[2]);
+                    IPADD.Text = value1 + "." + value2 + "." + value3 + ".";
+                }
+
+            if (IPADD.Text == "127.0.0.")
+            {
+                MessageBox.Show("No internet connection Found");
+            }
+            else
+            {
+                textBox1.Text = IPADD.Text;
+                backgroundWorker1.RunWorkerAsync();
+            }
+            string txt;
+            txt = IPADD.Text;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            option f2 = new option();
+            f2.Get_ip = this.dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            f2.Get_Host = this.dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            f2.ShowDialog();
+        }
+    }
+}
+ 
